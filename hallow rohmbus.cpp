a766a084990d4e1bb9887531eebeda8c06cc66d8
Joshithaaguru <joshithaaguru@gmail.com>
2025-03-12 19:23:58 +0530

    Add files via upload

---
+++ hallow rohmbus.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int n,j,i,s;
+	printf("enter the row:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		for(s=1;s<=n-i;s++){
+			printf(" ");
+		}
+		for(j=1;j<=(2*i-1);j++){
+		    if(j==1||j==(2*i-1)){ 
+		    	printf("*");
+		}else{
+		printf(" ");
+	    }
+	}
+	printf("\n");
+    }
+	for(i=n-1;i>=1;i--){
+		for(s=1;s<=n-i;s++){
+			printf(" ");
+		}
+		for(j=1;j<=(2*i-1);j++){
+			if(j==1||j==(2*i-1)){
+				printf("*");
+	    	}else{
+		       printf(" ");
+     	    }
+    	} 
+		printf("\n");
+  }
+	return 0;
+}
